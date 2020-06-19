99bba0845f00d9704285395fc0f9eb9ade590c23
carlos <edunogodz@gmail.com>
2020-06-19 14:36:04 -0300

    04 - Editing Data With Razor Pages

--- OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -9,6 +9,10 @@ namespace OdeToFood.Data
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetRestaurantByName(string name);
+        Restaurant GetById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+        int Commit();
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -22,7 +26,35 @@ namespace OdeToFood.Data
                 new Restaurant { Id = 2, Name = "Cinnamon Club", Location="London", Cuisine = CuisineType.Mexican},
                 new Restaurant { Id = 3, Name = "La Costa", Location="California", Cuisine = CuisineType.Indian},
             };
+        }        
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
         }
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location; 
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
         public IEnumerable<Restaurant> GetRestaurantByName(string name = null)
         {
             return from r in restaurants
